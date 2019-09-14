e2f5f07539435458fe7fa42a10120b1c2216ebeb
Philip Gatzka <philip.gatzka@mailbox.org>
2019-09-14 13:44:46 +0200

    Restart container automatically

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-docker run --rm -d -p 7624:7624 -p 9000:9000 \
+docker run -d -p 7624:7624 -p 9000:9000 \
   -v "indisrv_config:/root/.indi" \
   --name indisrv \
   --device /dev/bus/usb \
   --device /dev/ttyS0 \
   --device /dev/ttyUSB0 \
+  --restart="unless-stopped" \
   indisrv indi_asi_ccd indi_arduinost4 indi_simulator_telescope indi_nikon_ccd
