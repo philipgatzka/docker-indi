9ba71e08c3c644dbdbbfc807bf9947cc2e701ad9
Philip Gatzka <philip.gatzka@mailbox.org>
2019-12-11 22:15:58 +0100

    Simplify start script

--- start.sh
+++ start.sh
@@ -3,7 +3,5 @@ docker run -d -p 7624:7624 -p 9000:9000 \
   -v "indisrv_config:/root/.indi" \
   --name indisrv \
   --device /dev/bus/usb \
-  --device /dev/ttyS0 \
-  --device /dev/ttyACM0 \
-  --restart="unless-stopped" \
+  --restart="always" \
   indisrv indi_asi_ccd indi_nikon_ccd indi_lx200_OnStep
